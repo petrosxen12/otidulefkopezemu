a0db5982d4a5b41c382418b28cb236fcc96fd5a8
Petros Xenofontos <petros.x12@gmail.com>
2019-07-20 18:37:23 +0100

    Added new route for rgb data as it is real-time.

--- core/rpi_server.py
+++ core/rpi_server.py
@@ -2,15 +2,19 @@ from flask import request, Flask
 
 app = Flask(__name__)
 
+
 @app.route('/matrixdata', methods=['POST'])
 def matrix_data():
     if request.method == 'POST':
         data = request.get_json()
-        print(data)
         matrix = float(data.get('matrix'))
-        rgb = float(data.get('rgb'))
+        return {'matrix': matrix}
 
-        return {'rgb': rgb, 'matrix': matrix}
+    if request.method == 'GET':
+        data = request.get_json()
+        rgb = float(data.get('rgb'))
+        return {'rgb': rgb}
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
