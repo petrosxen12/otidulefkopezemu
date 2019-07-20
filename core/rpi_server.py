cd1c14d3ae0cedeacdf826df8dcfb3162ae2a38f
Petros Xenofontos <petros.x12@gmail.com>
2019-07-20 18:30:51 +0100

    Ekama pellara.

--- core/rpi_server.py
+++ core/rpi_server.py
@@ -12,5 +12,5 @@ def matrix_data():
 
         return {'rgb': rgb, 'matrix': matrix}
 
-    if __name__ == '__main__':
-        app.run(debug=True, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
