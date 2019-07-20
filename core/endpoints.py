a8d2102f2749f89a6fd97d561b44c038a712e98a
Petros Xenofontos <petros.x12@gmail.com>
2019-07-20 08:53:05 +0100

    Added data to endpoints.

--- core/endpoints.py
+++ core/endpoints.py
@@ -20,9 +20,23 @@ def startapp():
 def getFormData():
     if request.method == 'POST':
         data = request.form
-        messenger = request.form.get('allowMsgr')
-        facebook = request.form.get('allowFcbk')
-        # facebook = request.form.get('allowFcbk')
+        ninegag = data.get('9gag')
+        facebook = data.get('facebook')
+        wow = data.get('WoW')
+        fortnite = data.get('fortnite')
+        graph = data.get('graph')
+        info = data.get('info')
+        instagram = data.get('instagram')
+        minecraft = data.get('minecraft')
+        tumblr = data.get('tumblr')
+        letmewatchthis = data.get('letmewatchthis')
+        netflix = data.get('netflix')
+        steam = data.get('steam')
+        epic = data.get('epic')
+
+        num_cycle = data.get('num_cycle')
+        study_time = data.get('study_time')
+        break_time = data.get('break_time')
 
         return data
     else:
