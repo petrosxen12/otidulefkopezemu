893aa1894deb250c41c3e7a1365cdddffbe96cf8
Ectopic <ectopic.dax@gmail.com>
2019-07-20 10:36:14 +0300

    Serious code that Kassianizer fixed and improved. Lil buggy tho.@

--- core/data_handlers.py
+++ core/data_handlers.py
@@ -1,4 +1,5 @@
 import time
+import collections
 
 
 # import os
@@ -27,42 +28,62 @@ class DataHandler:
     takkonis_durations = []
     productive_durations = []
 
+    interval_takkonis_durations = collections.defaultdict(lambda: [0])
+    interval_productive_durations = collections.defaultdict(lambda: [0])
+    interval = 1
+
     check_point = 0
     last_check_point = 0
 
     rgb_metric = 0
+    matrix_metric = (1,1)
 
     takkonis = False
 
     rgb_metric_graph = []
     dt_graph = 0.5  # in seconds
+    duration = 240
+    matrix_interval = duration / 16
 
     def __init__(self, q):
         self.last_check_point = time.time()
+        self.start_time = self.last_check_point
         self.q = q
+        self.interval_start_time = self.last_check_point
+        self.interval_checkpoint = self.last_check_point
+        self.interval_last_checkpoint = self.last_check_point
 
         time.sleep(0.001)
 
     def update(self):
         # print("In update")
         self.check_point = time.time()
+        self.interval_checkpoint = self.check_point
         self.rgb_metric = self.current_productiveness()
+        self.matrix_metric = self.interval_productiveness()
 
         if not self.q.empty():
             print("----------------")
             print("Queue not empty\n\n")
             if self.takkonis:
                 self.takkonis_durations.append(self.check_point - self.last_check_point)
+                self.interval_takkonis_durations[self.interval-1].append(self.interval_checkpoint - self.interval_last_checkpoint)
 
             else:
                 self.productive_durations.append(self.check_point - self.last_check_point)
+                self.interval_productive_durations[self.interval-1].append(self.interval_checkpoint - self.interval_last_checkpoint)
+
+            # print(str(self.interval_takkonis_durations))
+            # print(str(self.interval_productive_durations))
 
             self.takkonis = self.q.get()
             self.last_check_point = self.check_point
+            self.interval_last_checkpoint = self.interval_checkpoint
             # if queue_variable == 'exit':
             #     self.exit_status = True
 
-        print(self.rgb_metric)
+        #print(self.rgb_metric)
+        print(self.matrix_metric)
 
     def current_productiveness(self):
         dt = self.check_point - self.last_check_point
@@ -75,3 +96,29 @@ class DataHandler:
         total_time = productive_time + takko_time + dt
 
         return (productive_time + (dt * int(not self.takkonis))) / total_time
+
+    def interval_productiveness(self):
+        dt = self.interval_checkpoint - self.interval_last_checkpoint
+
+        if dt > self.matrix_interval:
+            self.interval += 1
+            self.interval_start_time = self.check_point
+            self.interval_checkpoint = self.check_point
+            self.interval_last_checkpoint = self.check_point
+            dt = 0
+
+        if self.takkonis:
+            pass
+
+        #print("interval: " + str(self.interval))
+        productive_time = sum(self.interval_productive_durations[self.interval-1])
+        takko_time = sum(self.interval_takkonis_durations[self.interval-1])
+
+        #print(productive_time + dt * (int(not self.takkonis)))
+
+        #print(productive_time )#+ (dt * int(not self.takkonis)))
+
+        if (productive_time + (dt * int(not self.takkonis))) / self.matrix_interval > 1:
+            print("greater than zero")
+
+        return (self.interval,(productive_time + (dt * int(not self.takkonis))) / self.matrix_interval )
